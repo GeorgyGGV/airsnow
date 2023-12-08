b54661fc50bad5ccfaf215650b579a6ecc21915b
Georgy Gorgisheli <gorgisheli.g.v@gmail.com>
2023-12-08 11:36:55 +0300

    insert_dt_coalesce & insert_overwrite

--- dags/queries.py
+++ dags/queries.py
@@ -14,7 +14,6 @@ class Query:
                 OVER (PARTITION BY METADATA$FILENAME 
                 -- Order by row number to ensure headers are first in each window
                 ORDER BY METADATA$FILE_ROW_NUMBER) AS CSV_PARSER ) AS src ON V_TABLE.v: ID = src.v: ID
-                WHEN MATCHED THEN UPDATE SET  V_TABLE.v = src.v
                 WHEN NOT MATCHED THEN INSERT (V_TABLE.v) VALUES (src.v);
         """
         return sql
@@ -23,10 +22,7 @@ class Query:
     def load_col_table():
         sql = """
             MERGE INTO TASKS.TASK_1.COL_TABLE
-                USING TASKS.TASK_1.V_TABLE AS src ON COL_TABLE.ID = src.v:ID::INT
-                WHEN MATCHED THEN UPDATE SET  COL_TABLE.METACRITIC_RATING = src.v:METACRITIC_RATING,
-                                              COL_TABLE.REVIEWER_RATING = src.v:REVIEWER_RATING,
-                                              COL_TABLE.POSITIVITY_RATIO = src.v:POSITIVITY_RATIO                            
+                USING TASKS.TASK_1.V_TABLE AS src ON COL_TABLE.ID = src.v:ID::INT                           
                 WHEN NOT MATCHED THEN INSERT (COL_TABLE.ID, 
                                               COL_TABLE.NAME, 
                                               COL_TABLE.YEAR, 
@@ -37,36 +33,33 @@ class Query:
                                               COL_TABLE.TO_BEAT_EXTRA, 
                                               COL_TABLE.TO_BEAT_COMPLETIONIST, 
                                               COL_TABLE.EXTRA_CONTENT_LENGTH, 
-                                              COL_TABLE.TAGS) 
+                                              COL_TABLE.TAGS,
+                                              COL_TABLE.INSERT_DT
+                                              ) 
                                       VALUES (src.v: ID ,
-                                              src.v: NAME,
-                                              src.v: YEAR,
-                                              src.v: METACRITIC_RATING,
-                                              src.v: REVIEWER_RATING,
-                                              src.v: POSITIVITY_RATIO,
-                                              src.v: TO_BEAT_MAIN,
-                                              src.v: TO_BEAT_EXTRA,
-                                              src.v: TO_BEAT_COMPLETIONIST,
-                                              src.v: EXTRA_CONTENT_LENGTH,
-                                              src.v: TAGS);
+                                              COALESCE(src.v: NAME, 'n. a.'),
+                                              COALESCE(src.v: YEAR, -1),
+                                              COALESCE(src.v: METACRITIC_RATING, -1),
+                                              COALESCE(src.v: REVIEWER_RATING, -1),
+                                              COALESCE(src.v: POSITIVITY_RATIO, -1),
+                                              COALESCE(src.v: TO_BEAT_MAIN, -1),
+                                              COALESCE(src.v: TO_BEAT_EXTRA, -1),
+                                              COALESCE(src.v: TO_BEAT_COMPLETIONIST, -1),
+                                              COALESCE(src.v: EXTRA_CONTENT_LENGTH, -1),
+                                              src.v: TAGS,
+                                              CURRENT_DATE);
         """
         return sql
 
     @staticmethod
     def load_max_rating_table():
         sql = """
-            MERGE INTO TASKS.TASK_1.RATING_TABLE
-                USING (
+            INSERT OVERWRITE INTO TASKS.TASK_1.RATING_TABLE
                 SELECT YEAR, 
                        MAX(METACRITIC_RATING) AS max_metacritic_rating, 
                        MAX(REVIEWER_RATING) AS max_reviewer_rating
                 FROM TASKS.TASK_1.COL_TABLE
-                WHERE YEAR IS NOT NULL
+                WHERE YEAR != -1
                 GROUP BY YEAR 
-                ) AS src ON RATING_TABLE.YEAR = src.YEAR
-                WHEN MATCHED THEN UPDATE 
-                                      SET RATING_TABLE.MAX_METACRITIC_RATING = src.MAX_METACRITIC_RATING 
-                WHEN NOT MATCHED THEN INSERT (RATING_TABLE.YEAR, RATING_TABLE.MAX_METACRITIC_RATING, RATING_TABLE.MAX_REVIEWER_RATING )
-                                      VALUES (src.YEAR, src.MAX_METACRITIC_RATING, src.MAX_REVIEWER_RATING );
         """
         return sql
